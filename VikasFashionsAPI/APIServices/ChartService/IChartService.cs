16a6643bc9e06a9979a475ce694b0a82f87ea203
ayushmathur02 <ayush.mathur@mail.vinove.com>
2022-02-28 18:36:56 +0530

    Added comment

--- VikasFashionsAPI/APIServices/ChartService/IChartService.cs
+++ VikasFashionsAPI/APIServices/ChartService/IChartService.cs
@@ -5,7 +5,14 @@ namespace VikasFashionsAPI.APIServices.ChartService
     public interface IChartService
     {
         Task<IEnumerable<Chart>> GetAllAsync();
-        Task<Chart?> GetByIdAsync(int chartId);
+        Task<Chart?> GetByIdAsync(int chartId);        
+
+        /// <summary>
+        /// Save Chart Details
+        /// </summary>
+        /// <param name="chart">Object of type chart</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         Task<Chart> AddChartAsync(Chart Chart);
         Task<Chart?> UpdateChartAsync(Chart ChartChart);
         Task<bool> DeleteChartAsync(int chartId);
