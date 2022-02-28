d7064fdbe249bd4ea21186bf6e10758357f4d680
rajatgupta-vinove <rajat.gupta@mail.vinove.com>
2022-02-28 11:18:38 +0530

    state, city services

---
+++ VikasFashionsAPI/APIServices/CityService/ICityService.cs
@@ -0,0 +1,15 @@
+﻿using VikasFashionsAPI.Data;
+namespace VikasFashionsAPI.APIServices.CityService
+{
+    public interface ICityService
+    {
+        Task<IEnumerable<City>> GetAllAsync();
+        Task<IEnumerable<City>> GetAllByStatusAsync(bool? isActive);
+        Task<IEnumerable<City>> GetAllByStateIdAsync(int stateId);
+        Task<IEnumerable<City>> GetAllByStateAndStatusAsync(int stateId, bool? isActive);
+        Task<City?> GetByIdAsync(int CityId);
+        Task<City> AddCityAsync(City City);
+        Task<City?> UpdateCityAsync(City City);
+        Task<bool> DeleteCityAsync(int CityId);
+    }
+}
---
+++ VikasFashionsAPI/APIServices/StateService/IStateService.cs
@@ -0,0 +1,15 @@
+﻿using VikasFashionsAPI.Data;
+namespace VikasFashionsAPI.APIServices.StateService
+{
+    public interface IStateService
+    {
+        Task<IEnumerable<State>> GetAllAsync();
+        Task<IEnumerable<State>> GetAllByStatusAsync(bool? isActive);
+        Task<IEnumerable<State>> GetAllByCountryIdAsync(int countryId);
+        Task<IEnumerable<State>> GetAllByCountryAndStatusAsync(int countryId, bool? isActive);
+        Task<State?> GetByIdAsync(int StateId);
+        Task<State> AddStateAsync(State State);
+        Task<State?> UpdateStateAsync(State State);
+        Task<bool> DeleteStateAsync(int StateId);
+    }
+}
