858de45303bd0ba5536e160c6e73af5e06f68e9e
rajatgupta-vinove <rajat.gupta@mail.vinove.com>
2022-03-07 17:06:30 +0530

    added getall and getbyid method for user, in response we are sending JSON

--- VikasFashionsAPI/Common/CommonVars.cs
+++ VikasFashionsAPI/Common/CommonVars.cs
@@ -1,10 +1,43 @@
-﻿namespace VikasFashionsAPI.Common
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace VikasFashionsAPI.Common
 {
     public static class CommonVars
     {
         public static DateTime CurrentDateTime
-        { 
+        {
             get { return DateTime.UtcNow; }
         }
+
+        public enum MessageResults
+        {
+            [Display(Name = "Successfully saved!")]
+            [Description("Successfully saved!")]
+            SuccessSave,
+            [Display(Name = "Successfully updated!")]
+            [Description("Successfully updated!")]
+            SuccessUpdate,
+            [Display(Name = "Successfully get!")]
+            [Description("Successfully get!")]
+            SuccessGet,
+            [Display(Name = "Error while saving!")]
+            [Description("Error while saving!")]
+            ErrorSave,
+            [Display(Name = "Error while geting data!")]
+            [Description("Error while geting data!")]
+            ErrorGet,
+            [Display(Name = "User with this email already exists!")]
+            [Description("User with this email already exists!")]
+            UserDuplicateEmail,
+            [Display(Name = "User with this code already exists!")]
+            [Description("User with this code already exists!")]
+            UserDuplicateCode,
+            [Display(Name = "Invalid login details!")]
+            [Description("Invalid login details!")]
+            InvalidLogin
+        }
+
     }
 }
