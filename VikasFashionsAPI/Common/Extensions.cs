4af1a64082fe61f81e50a8ee90e8651126aea5ca
rajatgupta-vinove <rajat.gupta@mail.vinove.com>
2022-03-08 12:38:59 +0530

    changes related to handle CORS

--- VikasFashionsAPI/Common/Extensions.cs
+++ VikasFashionsAPI/Common/Extensions.cs
@@ -14,14 +14,21 @@ namespace VikasFashionsAPI.Common
         }
         public static string GetEnumDisplayName(this Enum value)
         {
-            FieldInfo fi = value.GetType().GetField(value.ToString());
+            FieldInfo? fi = value.GetType().GetField(value.ToString());
+            if (fi != null)
+            {
+                DisplayAttribute[] attributes = (DisplayAttribute[])fi.GetCustomAttributes(typeof(DisplayAttribute), false);
 
-            DisplayAttribute[] attributes = (DisplayAttribute[])fi.GetCustomAttributes(typeof(DisplayAttribute), false);
-
-            if (attributes != null && attributes.Length > 0)
-                return attributes[0].Name;
+                if (attributes != null && attributes.Length > 0)
+                    return attributes[0].Name;
+                else
+                    return value.ToString();
+            }
             else
+            {
                 return value.ToString();
+            }
+;
         }
     }
 }
